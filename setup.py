bc0a6ab363820d1477594c243ad83280538acbec
tstockdale <stockdale.thomas@gmail.com>
2025-07-25 11:55:24 -0400

    added type: ignore to setup import statement

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 Setup configuration for Weather Map Application.
 """
 
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages  # type: ignore
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
